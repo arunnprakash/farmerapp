c2e34a2428419647e5a2986039e34aab51b4e075
ArunPrakash <arunnprakash@gmail.com>
2020-03-07 19:38:43 +0530

    users fragment initial implementation

---
+++ app/src/main/java/com/grana/farmerapp/ui/users/UsersFragment.java
@@ -0,0 +1,38 @@
+package com.grana.farmerapp.ui.users;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.grana.farmerapp.R;
+
+public class UsersFragment extends Fragment {
+
+    private UsersViewModel mViewModel;
+
+    public static UsersFragment newInstance() {
+        return new UsersFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UsersViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/grana/farmerapp/ui/users/UsersViewModel.java
@@ -0,0 +1,7 @@
+package com.grana.farmerapp.ui.users;
+
+import androidx.lifecycle.ViewModel;
+
+public class UsersViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
