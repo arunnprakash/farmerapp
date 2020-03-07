20f602060851477a1ff065046f54f73358d0526e
ArunPrakash <arunnprakash@gmail.com>
2020-03-07 20:33:32 +0530

    users list initial implementation

--- app/src/main/java/com/grana/farmerapp/ui/users/UsersFragment.java
+++ app/src/main/java/com/grana/farmerapp/ui/users/UsersFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,7 +28,34 @@ public class UsersFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_users, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.users_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // this is data fro recycler view
+        ItemData itemsData[] = {
+                new ItemData("Indigo", R.drawable.circle),
+                new ItemData("Red", R.drawable.color_ic_launcher),
+                new ItemData("Blue", R.drawable.indigo),
+                new ItemData("Green", R.drawable.circle),
+                new ItemData("Amber", R.drawable.color_ic_launcher),
+                new ItemData("Deep Orange", R.drawable.indigo)
+        };
+
+
+        // 3. create an adapter
+        MyAdapter mAdapter = new MyAdapter(itemsData);
+        // 4. set adapter
+        recyclerView.setAdapter(mAdapter);
+        // 5. set item animator to DefaultAnimator
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        return rootView;
     }
 
     @Override
